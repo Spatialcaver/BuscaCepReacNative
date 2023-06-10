e2fee7c4ca13e19ebc91407bad89ababd91b775d
Spatialcaver <101186168+Spatialcaver@users.noreply.github.com>
2023-06-10 11:42:37 -0300

    commenting  this code

--- App.js
+++ App.js
@@ -1,70 +1,82 @@
+//Importando recursos necessários 
+
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from "react"
 import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View, Alert } from 'react-native';
 import Api from './src/services/api';
 
+//criando app
 export default function App() {
+  //definindo constantes para os campos iniciarem vazios.
   const[cep, setCep] = useState("")
   const[logradouro, setLogradouro] = useState("")
   const[localidade, setLocalidade] = useState("")
   const[Estado, setEstado] = useState("")
   const[Bairro, setBairro] =useState("")
 
+  //Criando função para busca e exibição dos dados solicitados a partir do cep inserido. 
 async function buscarCep(){
+  //Filtro para que o user não consiga enviar dados vazios.
   if(cep==""){
     Alert.alert("Cep inválido ou vazio!")
     setCep("")
   }
 try{
+  //captura do cep para consulta na Api, e exibição dos dados
     const response = await Api.get(`/${cep}/json/`)
+    //definindo os valores dos campos para os recebidos da Api
     setLogradouro(response.data.logradouro)
     setBairro(response.data.bairro)
     setEstado(response.data.uf)
     setLocalidade(response.data.localidade)
-
+//reporte de erro
 }catch(error){
   console.log("ERRO"+error)
 }
   
 }
 
+  //inicio da estrutura do app
   return (
     <View style={styles.container}>
     <View style={styles.topBar}>
         <Text style={styles.title}>Buscador de cep </Text>
     </View>
-<View style={styles.form}>
-  <TextInput
-  style={{borderColor:"#000000", borderWidth:2, width:200, fontSize:18, marginTop:20, marginEnd:20, borderRadius:10, padding:10}}
-  value={cep} onChangeText={(texto)=> setCep(texto)} placeholder='Cep'></TextInput>
+//campo cep 
+    <View style={styles.form}>
+        <TextInput
+        style={{borderColor:"#000000", borderWidth:2, width:200, fontSize:18, marginTop:20, marginEnd:20, borderRadius:10, padding:10}}
+        value={cep} onChangeText={(texto)=> setCep(texto)} placeholder='Cep'></TextInput>
+//botão de pesquisa
+        <TouchableOpacity style={styles.botao} onPress={buscarCep}>
+          <Text style={styles.textobuscar}>Buscar</Text>
+        </TouchableOpacity>
 
-  <TouchableOpacity style={styles.botao} onPress={buscarCep}>
-    <Text style={styles.textobuscar}>Buscar</Text>
-  </TouchableOpacity>
+     </View>
+//campo para exibição do logradouro
+      <TextInput
+        style={styles.caixadetexto}
+        value={logradouro} onChangeText={(texto)=> setLogradouro(texto)} placeholder='logradouro'></TextInput>
+//campo para exibição do Bairro
+      <TextInput
+        style={styles.caixadetexto}
+        value={Bairro} onChangeText={(texto)=> setBairro(texto)} placeholder='Bairro'></TextInput>
 
-</View>
-<TextInput
-  style={styles.caixadetexto}
-  value={logradouro} onChangeText={(texto)=> setLogradouro(texto)} placeholder='logradouro'></TextInput>
+//campo para exibição do municipio
+      <TextInput
+        style={styles.caixadetexto}
+        value={localidade} onChangeText={(texto)=> setLocalidade(texto)} placeholder='cidade'></TextInput>
 
-<TextInput
-  style={styles.caixadetexto}
-  value={Bairro} onChangeText={(texto)=> setBairro(texto)} placeholder='Bairro'></TextInput>
+//campo para exibição do estado
+      <TextInput
+        style={{borderColor:"#000000", borderWidth:2, width:80, fontSize:18, marginTop:10, marginEnd:10, borderRadius:10, padding:10, marginHorizontal:20}}
+        value={Estado} onChangeText={(texto)=> setEstado(texto)} placeholder='Estado'></TextInput>
 
+          </View>
+        );
+      }
 
-<TextInput
-  style={styles.caixadetexto}
-  value={localidade} onChangeText={(texto)=> setLocalidade(texto)} placeholder='cidade'></TextInput>
-
-<TextInput
-  style={{borderColor:"#000000", borderWidth:2, width:80, fontSize:18, marginTop:10, marginEnd:10, borderRadius:10, padding:10, marginHorizontal:20}}
-  value={Estado} onChangeText={(texto)=> setEstado(texto)} placeholder='Estado'></TextInput>
-
-    </View>
-  );
-}
-
-
+//inicio da estilização dos campos 
 const styles = StyleSheet.create({
   containerPrincipal: {
     flex: 1,
